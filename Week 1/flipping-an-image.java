f1a191aff27875934b01c4f1041ab20a36d0e859
aben-bel <abenezer.sbelay@gmail.com>
2020-06-10 23:31:47 +0300

    Array

---
+++ Week 1/flipping-an-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for(int i = 0; i<A.length; i++) {
+            for(int j = 0; j<A[i].length; j++) {
+                if(j >= A[i].length/2){
+                    if((A[i].length & 1) == 1){
+                        A[i][j] = A[i][j] == 0 ? 1 : 0;
+                    }
+                    break;
+                }
+                
+                
+                int temp = A[i][j];
+                A[i][j] = A[i][A[i].length - j - 1];
+                A[i][A[i].length - j - 1] = temp;
+                A[i][j] = A[i][j] == 0 ? 1 : 0;
+                A[i][A[i].length - j - 1] = A[i][A[i].length - j -1] == 0 ? 1 : 0;
+                
+            }
+        }
+        return A; 
+    }
+}
